877758853d0e83fb6c45bbf69e74f0e05b84d84e
KingCloudNinja <37353585+KingCloudNinja@users.noreply.github.com>
2018-03-18 23:32:07 -0400

    Added more arguments to the /broadcast command.

--- Server Essentials/src/tld/example/myplugin/Main.java
+++ Server Essentials/src/tld/example/myplugin/Main.java
@@ -228,8 +228,43 @@ If(args.length == 8){
 bukkit.broadcastMessage(config.getMainColor + config.getServerName + ChatColor.DARK_GRAY + "> " + args[0] + " " + args[1] + " " + args[2] + " " + args[3] + " " + args[4] + " " + args[5] + " " + args[6] + " " + args[7]);
 }
 If(args.length == 9){
-bukkit.broadcastMessage(config.getMainColor + config.getServerName + ChatColor.DARK_GRAY + "> " + args[0] + " " + args[1] + " " + args[2] + " " + args[3] + " " + args[4] + " " + args[5] + " " + args[6] + " " + args[7] + " " + args[8]); 
-		
+bukkit.broadcastMessage(config.getMainColor + config.getServerName + ChatColor.DARK_GRAY + "> " + args[0] + " " + args[1] + " " + args[2] + " " + args[3] + " " + args[4] + " " + args[5] + " " + args[6] + " " + args[7] + " " + args[8]);
+}
+if(args.length == 10){
+bukkit.broadcastMessage(config.getMainColor + config.getServerName + ChatColor.DARK_GRAY + "> " + args[0] + " " + args[1] + " " + args[2] + " " + args[3] + " " + args[4] + " " + args[5] + " " + args[6] + " " + args[7] + " " + args[8] + " " + args[9]);
+}
+if(args.length == 11){
+bukkit.broadcastMessage(config.getMainColor + config.getServerName + ChatColor.DARK_GRAY + "> " + args[0] + " " + args[1] + " " + args[2] + " " + args[3] + " " + args[4] + " " + args[5] + " " + args[6] + " " + args[7] + " " + args[8] + " " +
+ args[9] + " " + args[10]);
+}
+if(args.length == 12){
+bukkit.broadcastMessage(config.getMainColor + config.getServerName + ChatColor.DARK_GRAY + "> " + args[0] + " " + args[1] + " " + args[2] + " " + args[3] + " " + args[4] + " " + args[5] + " " + args[6] + " " + args[7] + " " + args[8] + " " +
+ args[9] + " " + args[10] + " " + args[11]);
+}
+if(args.length == 13){
+bukkit.broadcastMessage(config.getMainColor + config.getServerName + ChatColor.DARK_GRAY + "> " + args[0] + " " + args[1] + " " + args[2] + " " + args[3] + " " + args[4] + " " + args[5] + " " + args[6] + " " + args[7] + " " + args[8] + " " +
+ args[9] + " " + args[10] + " " + args[11] + " " + args[12]);
+}
+if(args.length == 14){
+bukkit.broadcastMessage(config.getMainColor + config.getServerName + ChatColor.DARK_GRAY + "> " + args[0] + " " + args[1] + " " + args[2] + " " + args[3] + " " + args[4] + " " + args[5] + " " + args[6] + " " + args[7] + " " + args[8] + " " +
+ args[9] + " " + args[10] + " " + args[11] + " " + args[12] + " " + args[13]);
+}
+if(args.length == 15){
+bukkit.broadcastMessage(config.getMainColor + config.getServerName + ChatColor.DARK_GRAY + "> " + args[0] + " " + args[1] + " " + args[2] + " " + args[3] + " " + args[4] + " " + args[5] + " " + args[6] + " " + args[7] + " " + args[8] + " " +
+ args[9] + " " + args[10] + " " + args[11] + " " + args[12] + " " + args[13] + " " + args[14]); 
+}
+if(args.length == 16){
+bukkit.broadcastMessage(config.getMainColor + config.getServerName + ChatColor.DARK_GRAY + "> " + args[0] + " " + args[1] + " " + args[2] + " " + args[3] + " " + args[4] + " " + args[5] + " " + args[6] + " " + args[7] + " " + args[8] + " " +
+ args[9] + " " + args[10] + " " + args[11] + " " + args[12] + " " + args[13] + " " + args[14] + " " + args[15]);
+}
+if(args.length == 17){
+bukkit.broadcastMessage(config.getMainColor + config.getServerName + ChatColor.DARK_GRAY + "> " + args[0] + " " + args[1] + " " + args[2] + " " + args[3] + " " + args[4] + " " + args[5] + " " + args[6] + " " + args[7] + " " + args[8] + " " +
+ args[9] + " " + args[10] + " " + args[11] + " " + args[12] + " " + args[13] + " " + args[14] + " " + args[15] + " " + args[16]);
+}
+if(args.length == 18){
+bukkit.broadcastMessage(config.getMainColor + config.getServerName + ChatColor.DARK_GRAY + "> " + args[0] + " " + args[1] + " " + args[2] + " " + args[3] + " " + args[4] + " " + args[5] + " " + args[6] + " " + args[7] + " " + args[8] + " " +
+ args[9] + " " + args[10] + " " + args[11] + " " + args[12] + " " + args[13] + " " + args[14] + " " + args[15] + " " + args[16] + " " + args[17]);
+} 
 	return false;
 	}
 
