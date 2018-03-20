9db81e65a96e419b002900ac210cf57b41598bc1
KingCloudNinja <37353585+KingCloudNinja@users.noreply.github.com>
2018-03-20 16:37:05 -0400

    Updated commands. & added config.

--- src/tld/example/myplugin/Main.java
+++ src/tld/example/myplugin/Main.java
@@ -1,16 +1,19 @@
 package tld.example.myplugin;
 
+import java.util.Arrays;
+
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.GameMode;
-import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
 public class Main extends JavaPlugin implements Listener {
 
@@ -21,30 +24,35 @@ public class Main extends JavaPlugin implements Listener {
 
 	public void onEnable() {
     	Bukkit.getPluginManager().registerEvents ((Listener) this, this);
-    	
+    	loadConfiguration();
 	}
 	
 	public void loadConfiguration(){
-	     String path = "Commands.ServerName";
-	     getConfig().addDefault(path, "I did!");
+	     String path = "Config.ServerName";	
+	     getConfig().addDefault(path, "Servername");
 	     getConfig().options().copyDefaults(true);
 	     saveConfig();
+	     getConfig().getString("Config.Servername");
+	}	
+	
+	public void onPlayerJoin(PlayerJoinEvent e) {
+		Player player = e.getPlayer();
+		
+		player.setPlayerListName(player.getDisplayName());
 	}
-
 	
 	
-	@EventHandler
-		
-		
+	
     public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
     
     	Player player = (Player) sender;
-    	Location location = player.getLocation();
     	this.getConfig();
+    	
     	FileConfiguration config = this.getConfig();
     	config.addDefault("servername", true);
     	config.addDefault("discord", true);
-    	String servername = (String) "ServerName";
+    	String servername = (String) getConfig().getString("Config.Servername");
+    	
     	
     	if(commandLabel.equalsIgnoreCase("trail")) {
         	player.performCommand("trails");
@@ -58,7 +66,7 @@ public class Main extends JavaPlugin implements Listener {
 		
 	    	}
 	    		if(commandLabel.equalsIgnoreCase("about")) {
-		    		player.sendMessage(ChatColor.DARK_RED + "ServerEssentials" + ChatColor.DARK_GRAY + "> " + ChatColor.GRAY + "ServerEssentials Plugin created by: KingCloudNinja. Get more plugins here: https://discord.gg/zGTRYuh");
+		    		player.sendMessage(ChatColor.DARK_RED + "ServerEssentials" + ChatColor.DARK_GRAY + "> " + ChatColor.GRAY + "ServerEssentials Plugin created by: " + ChatColor.GREEN + "KingCloudNinja" + ChatColor.GRAY + ". Get more plugins here: " + ChatColor.GREEN + "https://discord.gg/zGTRYuh");
 		    	}
 			if(commandLabel.equalsIgnoreCase("spec") || commandLabel.equalsIgnoreCase("spectator")){
 				Player target = Bukkit.getServer().getPlayer(args[0]);
@@ -73,31 +81,18 @@ public class Main extends JavaPlugin implements Listener {
 				player.sendMessage(ChatColor.GRAY + "You are spectating " + ChatColor.GREEN + args[0] + ChatColor.GRAY + "." );
 			}
 			}
-			if(commandLabel.equalsIgnoreCase("v")){
-				player.setGameMode(GameMode.SPECTATOR);
-				player.sendMessage(ChatColor.GRAY + "You are now " + ChatColor.GREEN + "Vanished" + ChatColor.GRAY + ".");
-				location.getBlock().getLocation();
-			}
-			if(commandLabel.equalsIgnoreCase("unv")){
-				player.setGameMode(GameMode.SURVIVAL);
-				player.sendMessage(ChatColor.GRAY + "You are now " + ChatColor.GREEN + "Unvanished" + ChatColor.GRAY + ".");
-				player.teleport(location);
-			
-		
-		}
-	
-		
+
 	if(commandLabel.equalsIgnoreCase("fakeleave")){
 		player.setGameMode(GameMode.SPECTATOR); 
 		Bukkit.broadcastMessage(ChatColor.YELLOW + player.getName() + ChatColor.YELLOW + " left the game");
 		player.sendMessage(ChatColor.GRAY + "You are now " + ChatColor.GREEN + "Vanished" + ChatColor.GRAY + ".");
-		player.performCommand("ev");
+		player.setPlayerListName(null);
 	}
-		if(commandLabel.equalsIgnoreCase("unfakeleave")){
+		if(commandLabel.equalsIgnoreCase("fakejoin")){
     		player.setGameMode(GameMode.SURVIVAL);
     		Bukkit.broadcastMessage(ChatColor.YELLOW + player.getName() + ChatColor.YELLOW + " joined the game");
 			player.sendMessage(ChatColor.GRAY + "You are now " + ChatColor.GREEN + "Unvanished" + ChatColor.GRAY + ".");
-			player.performCommand("ev");
+			player.setPlayerListName(player.getDisplayName());
 			
     	
 	}
@@ -107,98 +102,6 @@ public class Main extends JavaPlugin implements Listener {
 		if(commandLabel.equalsIgnoreCase("shrug")){
 			Bukkit.broadcastMessage(ChatColor.WHITE + "<" + player.getDisplayName() + ">" + ChatColor.BOLD + " Shrugged");
 	}
-		if(commandLabel.equalsIgnoreCase("ban")){
-			Player target = Bukkit.getServer().getPlayer(args[0]);
-			if(args.length == 0 || args.length == 1){
-				player.sendMessage(ChatColor.RED + "Usage: /ban (player) Reason");
-			}
-				
-			if(args[1].equals("hacking") || args[1].equals("threat") || args[1].equals("threats")){
-				if(args.length == 2){
-				player.performCommand("eban " + target.getName());
-				Bukkit.broadcastMessage(player.getName() + ChatColor.GREEN + " banned " + ChatColor.WHITE + target.getName() + ChatColor.GREEN + " for " + args[1]);
-			}
-				else{
-					if(args[3].equals("-s")){
-						player.performCommand("eban " + target.getName());
-					}
-				}
-				
-			if(args[1].equals("evasion")){
-				if(args.length == 2){
-					player.performCommand("eban " + target.getName());
-					Bukkit.broadcastMessage(player.getName() + ChatColor.GREEN + " banned " + ChatColor.WHITE + target.getName() + ChatColor.GREEN + " for " + args[1]);
-				}
-					else{
-						if(args[3].equals("-s")){
-							player.performCommand("eban " + target.getName());
-						}
-					}
-			}
-			if(args[1].equals("abuse")){
-				if(args.length == 2){
-					player.performCommand("eban " + target.getName());
-					Bukkit.broadcastMessage(player.getName() + ChatColor.GREEN + " banned " + ChatColor.WHITE + target.getName() + ChatColor.GREEN + " for " + args[1]);
-				}
-					else{
-						if(args[3].equals("-s")){
-							player.performCommand("eban " + target.getName());
-						}
-					}
-			}
-			if(args[1].equals("perm")){
-				if(args.length == 2){
-					player.performCommand("eban " + target.getName());
-					Bukkit.broadcastMessage(player.getName() + ChatColor.GREEN + " banned " + ChatColor.WHITE + target.getName() + ChatColor.GREEN + " for " + args[1]);
-				}
-					else{
-						if(args[3].equals("-s")){
-							player.performCommand("eban " + target.getName());
-						}
-					}
-			}
-	}
-		}
-		if(commandLabel.equalsIgnoreCase("mute")){
-			Player target = Bukkit.getServer().getPlayer(args[0]);
-			if(args.length == 0 || args.length == 1){
-				player.sendMessage(ChatColor.RED + "Usage: /ban (player) Reason"); 
-			}
-			if(args[1].equals("cussing") || args[1].equals("cursing") || args[1].equals("language") || args[1].equals("impersonation") || args[1].equals("impersonating")){
-				if(args.length == 2){
-					player.performCommand("emute " + target.getName());
-					Bukkit.broadcastMessage(player.getName() + ChatColor.GREEN + " muted " + ChatColor.WHITE + target.getName() + ChatColor.GREEN + " for " + args[1]);
-				}
-					else{
-						if(args[3].equals("-s")){
-							player.performCommand("emute " + target.getName());
-						}
-					}
-			}
-			if(args[1].equals("advertising") || args[1].equals("ad") || args[1].equals("ads") || args[1].equals("threat") || args[1].equals("threats")){
-				if(args.length == 2){
-					player.performCommand("emute " + target.getName());
-					Bukkit.broadcastMessage(player.getName() + ChatColor.GREEN + " muted " + ChatColor.WHITE + target.getName() + ChatColor.GREEN + " for " + args[1]);
-				}
-					else{
-						if(args[3].equals("-s")){
-							player.performCommand("emute " + target.getName());
-						}
-					}
-			}
-			if(args[1].equals("rude") || args[1].equals("disrespect") || args[1].equals("disrespective")){
-				if(args.length == 2){
-					player.performCommand("emute " + target.getName());
-					Bukkit.broadcastMessage(player.getName() + ChatColor.GREEN + " muted " + ChatColor.WHITE + target.getName() + ChatColor.GREEN + " for " + args[1]);
-				}
-					else{
-						if(args[3].equals("-s")){
-							player.performCommand("emute " + target.getName());
-						}
-					}
-			}
-	}
-		
 		if(commandLabel.equalsIgnoreCase("bc") || commandLabel.equalsIgnoreCase("broadcast")){
 			if(args.length == 0){
 				player.sendMessage(ChatColor.RED + "Not enough args. Usage: '/bc (message)'");
@@ -208,43 +111,16 @@ public class Main extends JavaPlugin implements Listener {
 					Bukkit.broadcastMessage(ChatColor.AQUA + servername  + ChatColor.DARK_GRAY + "> " + ChatColor.GREEN + broadcastMessage);
 		}
 		}
-		if(commandLabel.equalsIgnoreCase("serveressentials") || commandLabel.equalsIgnoreCase("se")){
-			if(args[0] == "reload"){
-				//plugin.reloadConfig()
-			}
-		}
-		if(commandLabel.equalsIgnoreCase("fly")){
-			if(player.isFlying()){
-			player.setFlying(false);
-			}
-			if(!player.isFlying()){
-			player.setFlying(true);
-			}
-			if(player.hasPermission("server.togglefly")){
-			if(args.length == 1){
-			Player target = Bukkit.getServer().getPlayer(args[0]);
-				if(target.isFlying()){
-					target.setFlying(false);
-			}
-			if (!target.isFlying()){
-			target.setFlying(true);
-			}
-			}
-			}
-			else{
-				player.sendMessage(ChatColor.RED + "Usage: /fly OR /fly (player)");
-			}
-		}
 		if(commandLabel.equalsIgnoreCase("feed") || commandLabel.equalsIgnoreCase("eat")){
 			if(args.length == 0){
 			player.setFoodLevel(20);
 			player.sendMessage(ChatColor.GRAY + "Your appetite has been sated.");
 			}
-			if(args.length >= 1){
+			if(args.length == 1){
 				Player target = Bukkit.getServer().getPlayer(args[0]);
 				target.setFoodLevel(20);
-				target.sendMessage(ChatColor.GRAY + "Your appetite has been sated by " + player.getName());
-				player.sendMessage(ChatColor.GRAY + "You fed " + target.getName());
+				target.sendMessage(ChatColor.GRAY + "Your appetite has been sated by " + ChatColor.GREEN + player.getName());
+				player.sendMessage(ChatColor.GRAY + "You fed " + ChatColor.GREEN + target.getName());
 			}
 			if(args.length >= 2){
 				player.sendMessage(ChatColor.RED + "Too many arguments. Usage: /heal OR /heal (player)");
@@ -256,17 +132,32 @@ public class Main extends JavaPlugin implements Listener {
 			player.setHealth(20);
 			player.sendMessage(ChatColor.GRAY + "You have been healed.");
 			player.getActivePotionEffects().remove(true);
+			player.setFoodLevel(20);
 			}
-			if(args.length >= 1){
+			if(args.length == 1){
 				Player target = Bukkit.getServer().getPlayer(args[0]);
 				target.setHealth(20);
-				target.sendMessage(ChatColor.GRAY + "You have been healed by " + player.getName());
-				player.sendMessage(ChatColor.GRAY + "You healed " + target.getName());
+				target.sendMessage(ChatColor.GRAY + "You have been healed by " + ChatColor.GREEN + player.getName());
+				player.sendMessage(ChatColor.GRAY + "You healed " + ChatColor.GREEN + target.getName());
 				target.getActivePotionEffects().remove(true);
+				target.setFoodLevel(20);
 			}
 			if(args.length >= 2){
 				player.sendMessage(ChatColor.RED + "Too many arguments. Usage: /heal OR /heal (player)");
 			}
+		}
+		if(commandLabel.equalsIgnoreCase("se") || commandLabel.equalsIgnoreCase("serveressentials")){
+			if(args[0].equals("reload")){
+				reloadConfig();
+				player.sendMessage(ChatColor.GREEN + "Config Reloaded!");
+			}
+			if(args[0].equals("help")){
+				player.sendMessage(ChatColor.GREEN + "Commands: /se reload /se help");
+			}
+			else{
+				player.sendMessage(ChatColor.RED + "Command not found! Do /se help for correct commands.");
+			}
+			
 		}
 	return false;
 	}
